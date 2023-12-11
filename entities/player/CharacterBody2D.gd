5f192cb3bf35e5bf7d0dbf239aeed1efdae072ad
Andrej-sv81 <mitrovic.sv81.2021@uns.ac.rs>
2023-12-11 17:11:01 +0100

    Added double jump and test paltforms

--- entities/player/CharacterBody2D.gd
+++ entities/player/CharacterBody2D.gd
@@ -1,9 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
-
+const SPEED = 100.0
+const JUMP_VELOCITY = -150.0
+var double_jumped  = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
@@ -14,9 +14,13 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+			double_jumped = false
+		elif not double_jumped:
+			velocity.y = JUMP_VELOCITY
+			double_jumped = true
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
